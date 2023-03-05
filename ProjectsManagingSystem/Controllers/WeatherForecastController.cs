6610d2e1dc5fa4636c6fda657ef6a1ac9adac88c
micha <harasim1999@gmail.pl>
2023-03-05 21:15:11 +0100

    eeee

--- ProjectsManagingSystem/Controllers/WeatherForecastController.cs
+++ ProjectsManagingSystem/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
     {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","ee"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","eee"
     };
 
     private readonly ILogger<WeatherForecastController> _logger;
