a22d4ac1dc83e77fc7d041b8ecf8bdf09ad11b19
AmpF5 <lolc44t@gmail.com>
2023-03-07 18:57:03 +0100

    PMS-7

---
+++ ProjectsManagingSystem/Entities/Project.cs
@@ -0,0 +1,12 @@
+﻿namespace ProjectsManagingSystem.Entities;
+
+public class Project
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public DateTime Deadline { get; set; }
+    public List<ProjectTask> Tasks { get; set; }
+    public List<Member> Members { get; set; }
+    public DateTime DateOfCreation { get; set; }
+}
