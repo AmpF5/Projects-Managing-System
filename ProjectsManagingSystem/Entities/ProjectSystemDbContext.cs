4319c5a8b7e2e625aa17c6c39ec1a1c143b7e94a
AmpF5 <lolc44t@gmail.com>
2023-03-08 19:26:53 +0100

    PMS-14 v2

---
+++ ProjectsManagingSystem/Entities/ProjectSystemDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProjectsManagingSystem.Entities;
+
+public class ProjectSystemDbContext : DbContext
+{
+    public DbSet<Project> Projects { get; set; }
+    public DbSet<Member> Members { get; set; }
+    public DbSet<ProjectTask> ProjectTasks { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"Server=(localdb)\MVC;Database=ProjectManagingSystemDB;Trusted_Connection=True");
+    }
+}
