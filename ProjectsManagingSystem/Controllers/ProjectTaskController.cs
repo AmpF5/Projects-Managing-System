1e29e16eda39f384f3c7b44fddbeebb613d24690
micha <harasim1999@gmail.pl>
2023-03-13 15:23:11 +0100

    solve

---
+++ ProjectsManagingSystem/Controllers/ProjectTaskController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectsManagingSystem.Entities;
+using ProjectsManagingSystem.Services;
+
+namespace ProjectsManagingSystem.Controllers
+{
+    [Route("api/project/task")]
+    [ApiController]
+    public class ProjectTaskController: ControllerBase
+
+    {
+        private readonly IProjectTaskService _projectTaskService;
+
+        public ProjectTaskController(IProjectTaskService projectTaskService)
+        {
+            _projectTaskService = projectTaskService;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Task>> GetAll()
+        {
+            var tasks = _projectTaskService.GetAll();
+
+            return Ok(tasks);
+
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult GetById(int id)
+        {
+            var task = _projectTaskService.GetTask(id);
+
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(task);
+
+        }
+
+    }
+}
---
+++ ProjectsManagingSystem/Services/IProjectTaskService.cs
@@ -0,0 +1,10 @@
+﻿using ProjectsManagingSystem.Entities;
+
+namespace ProjectsManagingSystem.Services
+{
+    public interface IProjectTaskService
+    {
+        IEnumerable<ProjectTask> GetAll();
+        ProjectTask GetTask(int id);
+    }
+}
