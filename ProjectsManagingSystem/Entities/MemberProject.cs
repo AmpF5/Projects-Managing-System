4e60beff046da1f6972349da305a623c1508d977
AmpF5 <lolc44t@gmail.com>
2023-03-19 17:16:52 +0100

    PMS-31 migration

--- ProjectsManagingSystem/Entities/MemberProject.cs
+++ ProjectsManagingSystem/Entities/MemberProject.cs
@@ -1,7 +1,9 @@
-﻿namespace ProjectsManagingSystem.Entities;
+﻿using Microsoft.EntityFrameworkCore;
 
+namespace ProjectsManagingSystem.Entities;
 public class MemberProject
 {
+    public int Id { get; set; }
     public int MemberId { get; set; }
     public Member Member { get; set; }
     public int ProjectId { get; set; }
